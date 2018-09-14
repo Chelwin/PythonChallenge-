7a790025133d955aee6f420ae7ffac525755fd70
Chelwin <Chelwin@users.noreply.github.com>
2018-09-14 11:17:22 +0530

    Create level1.py

---
+++ level1.py
@@ -0,0 +1,34 @@
+# http://www.pythonchallenge.com/pc/def/map.html
+
+
+s = 'g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj.'
+'''
+a c
+b d
+c e
+d f
+e g
+f h
+g i
+h j
+i k
+j l
+k m
+l n
+m o
+n p
+o q
+p r
+q s
+r t
+s u
+t v
+u w
+v x
+w y
+x z
+y a
+z b
+'''
+
+
